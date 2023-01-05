8a08fee9a7fe9b1498ec58294bd31ca974a6d5e3
henryvanner <henry.vanner@gmail.com>
2023-01-04 20:23:47 -0500

    feat: setup firs test

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+
+test('Home page is complete',() => {
+    render(<Home />)
+    expect(screen.getByText('Hello World')).toBeInTheDocument()
+})
