4396280c4cf79cf44697210b5a394b17282ca16e
henryvanner <henry.vanner@gmail.com>
2023-01-18 15:53:11 -0500

    style: update headings and illustration for larger screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,17 @@ import HomeIllustration from '../public/Home_illustration.svg'
 export default function Home() {
   return (
     <div className='Home'>
-      <h1 className='Home__Name'>HENRY AQUINO</h1>
-      <h2 className='Home__ProfessionTitle'>Informatic Engineer</h2>
-      <Image
-        className='Home__Illustration'
-        src={HomeIllustration}
-        alt='Funcy Desktop'
-      />
+      <div className='Home__HeadingsAndIllustration'>
+        <div>
+          <h1 className='Home__Name'>HENRY AQUINO</h1>
+          <h2 className='Home__ProfessionTitle'>Informatic Engineer</h2>
+        </div>
+        <Image
+          className='Home__Illustration'
+          src={HomeIllustration}
+          alt='Funcy Desktop'
+        />
+      </div>
       <div className='Home__Buttons'>
         <button className='Button Button--red'>PROJECTS</button>
         <button className='Button Button--blue'>CURRICULUM</button>
