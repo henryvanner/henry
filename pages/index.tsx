c3e5b9aac31fd357d3fcd504eb68d2698731e7cc
henryvanner <henry.vanner@gmail.com>
2023-02-04 00:58:07 -0500

    feat: add waves illustrations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import HomeIllustration from '../public/Home_illustration.svg'
+import Wave1Illustration from '../public/Wave_1_illustration.png'
+import Wave2Illustration from '../public/Wave_2_illustration.png'
+import Wave3Illustration from '../public/Wave_3_illustration.png'
 
 export default function Home() {
   return (
@@ -19,6 +22,23 @@ export default function Home() {
         <div className='Home__Buttons'>
           <button className='Button Button--red'>PROJECTS</button>
           <button className='Button Button--blue'>CURRICULUM</button>
+        </div>
+        <div className='Home__WavesIllustration'>
+          <Image
+            className='Home__WaveIllustration'
+            src={Wave1Illustration}
+            alt='Waves'
+          />
+          <Image
+            className='Home__WaveIllustration Home__WaveIllustration--middle'
+            src={Wave2Illustration}
+            alt='Waves'
+          />
+          <Image
+            className='Home__WaveIllustration'
+            src={Wave3Illustration}
+            alt='Waves'
+          />
         </div>
       </div>
     </div>
