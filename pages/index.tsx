d72f543ae2bf54e7e61dee5b9dafeb1317e68c66
henryvanner <henry.vanner@gmail.com>
2023-02-03 23:09:35 -0500

    feat: introduce paper class

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,23 @@ import HomeIllustration from '../public/Home_illustration.svg'
 
 export default function Home() {
   return (
-    <div className='Home'>
-      <div className='Home__HeadingsAndIllustration'>
-        <div>
-          <h1 className='Home__Name'>HENRY AQUINO</h1>
-          <h2 className='Home__ProfessionTitle'>Informatic Engineer</h2>
+    <div className='Paper'>
+      <div className='Home'>
+        <div className='Home__HeadingsAndIllustration'>
+          <div>
+            <h1 className='Home__Name'>HENRY AQUINO</h1>
+            <h2 className='Home__ProfessionTitle'>Informatic Engineer</h2>
+          </div>
+          <Image
+            className='Home__Illustration'
+            src={HomeIllustration}
+            alt='Funcy Desktop'
+          />
         </div>
-        <Image
-          className='Home__Illustration'
-          src={HomeIllustration}
-          alt='Funcy Desktop'
-        />
-      </div>
-      <div className='Home__Buttons'>
-        <button className='Button Button--red'>PROJECTS</button>
-        <button className='Button Button--blue'>CURRICULUM</button>
+        <div className='Home__Buttons'>
+          <button className='Button Button--red'>PROJECTS</button>
+          <button className='Button Button--blue'>CURRICULUM</button>
+        </div>
       </div>
     </div>
   )
